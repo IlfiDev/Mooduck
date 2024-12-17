ce8cd881b8b01c101b2e6f6dc1e8c9ab67bd37bb
filippov_ia <filippov_ia@resanta.ru>
2024-12-17 22:59:04 +0300

    Small fix

--- composeApp/src/commonMain/kotlin/org/ilfidev/mooduck/Cards.kt
+++ composeApp/src/commonMain/kotlin/org/ilfidev/mooduck/Cards.kt
@@ -12,13 +12,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -30,7 +36,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun MainCard(data: MainCardData) {
     val likes = data.likesCount
     Card(
-        shape = RoundedCornerShape(10.dp),
+        shape = RoundedCornerShape(0.dp),
         modifier = Modifier.wrapContentWidth().shadow(0.dp, shape = RectangleShape).clickable {},
         backgroundColor = Color(0xFFd8d8d8)
     ) {
@@ -50,7 +56,7 @@ fun MainCardHeader(title: String, likesCount: Int) {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        WrappedTextWithMaxWidth(title)
+//        WrappedTextWithMaxWidth(title)
         Text(
             modifier = Modifier.widthIn(100.dp, 300.dp),
             text = title,
@@ -69,17 +75,21 @@ fun MainCardHeader(title: String, likesCount: Int) {
 
 @Composable
 fun MainCardContent(description: String, imageUrl: String?) {
-    Column(modifier = Modifier.fillMaxSize().padding(vertical = 23.dp, horizontal = 15.dp)) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(vertical = 23.dp, horizontal = 15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
 
         AsyncImage(
             model = imageUrl,
             contentDescription = "",
             modifier = Modifier
-                .width(300.dp).height(300.dp)
+                .fillMaxSize()
                 .border(
-                    border = BorderStroke(width = 1.dp, Color.Black)
+                    border = BorderStroke(width = 3.dp, Color.White)
                 ),
-            contentScale = ContentScale.Fit
+            contentScale = ContentScale.Crop
         )
 
         Text(description)
@@ -106,3 +116,73 @@ fun WrappedTextWithMaxWidth(text: String) {
         )
     }
 }
+
+
+
+//fun Modifier.innerShadow(
+//    color: Color = Color.Black,
+//    cornersRadius: Dp = 0.dp,
+//    spread: Dp = 0.dp,
+//    blur: Dp = 0.dp,
+//    offsetY: Dp = 0.dp,
+//    offsetX: Dp = 0.dp
+//) = drawWithContent {
+//
+//    drawContent()
+//
+//    val rect = Rect(Offset.Zero, size)
+//    val paint = Paint()
+//
+//    drawIntoCanvas {
+//
+//        paint.color = color
+//        paint.isAntiAlias = true
+//        it.saveLayer(rect, paint)
+//        it.drawRoundRect(
+//            left = rect.left,
+//            top = rect.top,
+//            right = rect.right,
+//            bottom = rect.bottom,
+//            cornersRadius.toPx(),
+//            cornersRadius.toPx(),
+//            paint
+//         )
+//         val frameworkPaint = paint.asFrameworkPaint()
+//         frameworkPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
+//         if (blur.toPx() > 0) {
+//             frameworkPaint.maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
+//         }
+//         val left = if (offsetX > 0.dp) {
+//             rect.left + offsetX.toPx()
+//         } else {
+//             rect.left
+//         }
+//         val top = if (offsetY > 0.dp) {
+//             rect.top + offsetY.toPx()
+//         } else {
+//             rect.top
+//         }
+//         val right = if (offsetX < 0.dp) {
+//             rect.right + offsetX.toPx()
+//         } else {
+//             rect.right
+//         }
+//         val bottom = if (offsetY < 0.dp) {
+//             rect.bottom + offsetY.toPx()
+//         } else {
+//             rect.bottom
+//         }
+//         paint.color = Color.Black
+//         it.drawRoundRect(
+//             left = left + spread.toPx() / 2,
+//             top = top + spread.toPx() / 2,
+//             right = right - spread.toPx() / 2,
+//             bottom = bottom - spread.toPx() / 2,
+//             cornersRadius.toPx(),
+//             cornersRadius.toPx(),
+//             paint
+//         )
+//         frameworkPaint.xfermode = null
+//         frameworkPaint.maskFilter = null
+//    }
+//}
